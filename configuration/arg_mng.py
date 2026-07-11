23b972f6a779702a9924187bf033437ed618b1e9
hamidm317 <hamidm317@gmail.com>
2026-07-11 13:25:41 +0330

    Create arg_mng.py (general parameters of saving plots)

---
+++ configuration/arg_mng.py
@@ -0,0 +1,24 @@
+dpi = 1200
+SHOW = False
+SAVE = True
+MAIN_DIR = ''
+FORMAT = 'png'
+
+FIG_NAMES = {
+
+    'main-02': 'fig02_power_ctrl_all_trials',
+    'main-03': 'fig03_dpli_ctrl_all_trials',
+    'main-04': 'fig04_dpli_ctrl_vs_dep_across_all_trials',
+    'main-05': 'fig05_power_both_groups_early_late_trials',
+    'main-06': 'fig06_dpli_both_groups_early_late_trials',
+  
+    'supp-02': 'figs02_both_groups_erp_all_trials', 
+    'supp-03': 'figs03_power_dep_all_trials', 
+    'supp-04': 'figs04_dpli_dep_all_trials', 
+    'supp-05': 'figs05_dpli_ctrl_all_trials_put_top_down_bottom_up', 
+    'supp-06': 'figs06_dpli_dep_all_trials_put_top_down_bottom_up', 
+    'supp-07': 'figs07_dpli_both_groups_early_vs_late_trials', 
+    'supp-08': 'figs08_dpli_both_groups_early_late_trials_stimulus_onset',
+    'supp-09': 'figs09_dpli_vs_learning_rate_correlation_all_ctrl_dep',
+
+}
